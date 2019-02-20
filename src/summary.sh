44cb080efdeac26fa889fbf4f7b6b0de27f6419b
ilker Gundogdu <ilker.gundogdu@gmail.com>
2019-02-20 12:41:31 +0300

    Add top 10 theta_star to summary.sh

--- src/summary.sh
+++ src/summary.sh
@@ -1,3 +1,10 @@
+echo 'First 10 components of theta_star'
+grep star $1 | cut -d' ' -f4| cut -d',' -f1-10
+
+echo ''
+
+echo 'Top 10 presentations'
+
 grep largest $1 | cut -d' ' -f5 \
     | awk '{
     split($0, arr, ",");
